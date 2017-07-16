4af56566390746b8e51f9a5d8f783564d3f723f0
심명훈 <julsnutini@naver.com>
2017-07-16 13:56:04 +0900

    SNS 공유 메뉴 만듦

---
+++ app/src/main/java/com/example/mh/cb_sports_festival/KakaoSharing.java
@@ -0,0 +1,12 @@
+package com.example.mh.cb_sports_festival;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KakaoSharing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/mh/cb_sports_festival/MainActivity.java
+++ app/src/main/java/com/example/mh/cb_sports_festival/MainActivity.java
@@ -9,15 +9,22 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TabHost;
+import android.widget.Toast;
+
+import com.kakao.kakaolink.KakaoLink;
+import com.kakao.kakaolink.KakaoTalkLinkMessageBuilder;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     ViewPager viewPager;
+    ImageButton share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,26 @@ public class MainActivity extends AppCompatActivity
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(viewPagerAdapter);
 
+        share = (ImageButton) findViewById (R.id.share);
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭시 팝업 메뉴가 나오게 하기
+                // PopupMenu 는 API 11 레벨부터 제공한다
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, share);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                popupMenu.show(); // 메뉴를 띄우기
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -74,7 +101,7 @@ public class MainActivity extends AppCompatActivity
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://tour.chungbuk.go.kr/home/sub.php?menukey=225"));
         startActivity(intent);
     }
-    public void onButtonHomeClicked(View V) { //맛집 숙박 사이트(충북 나드리)로 넘어감
+    public void onButtonHomeClicked(View V) { //홈으로 이동
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
@@ -128,4 +155,37 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.END);
         return true;
     }
+
+    public void shareKakao(){
+        try{
+            final KakaoLink kakaoLink = KakaoLink.getKakaoLink(this);
+            final KakaoTalkLinkMessageBuilder kakaoBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
+
+            //메시지 추가
+            kakaoBuilder.addText("전국체전 홍보 앱 모두의 체전입니다.");
+
+            //이미지 가로 세로 는 80px보다 커야함. 이미지 용량은 500Kb 이하
+            String url="https://lh5.ggpht.com/yVfPv-yLjIuBjpKj41NLkLXmuVv8XzH0m2hf-_sz9lQDv9WB9SX0McB8Jn4bQe4w5Q=w300";
+            kakaoBuilder.addImage(url,30,30);
+
+            //앱 실행버튼
+            kakaoBuilder.addAppButton("앱 실행, 다운하기");
+
+            //메시지 발송
+            kakaoLink.sendMessage(kakaoBuilder, this);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }/*
+    @Override
+    public boolean onOptionsItemSelected2(MenuItem item){
+        switch (item.getItemId())
+        {
+            case R.id.kakaoshare:
+                shareKakao();
+                break;
+        }
+        return super.onOptionsItemSelected2(item);
+    }*/
 }
